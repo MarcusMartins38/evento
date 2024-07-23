c0b83baa15b2497c99cff3e30b297631a38646dd
unknown <marcusmartins38@gmail.com>
2024-07-23 00:10:05 -0300

    feat: styling header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,37 @@
 import Link from "next/link";
 import Logo from "./logo";
 
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Home",
+    path: "/events/all",
+  },
+];
+
 export default function Header() {
   return (
-    <header>
+    <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <Link href="/">Home</Link>
-      <Link href="/events/all">All Events</Link>
+
+      <nav className="h-full">
+        <ul className="flex items-center gap-x-6 h-full text-sm">
+          {routes.map((route) => (
+            <li
+              key={route.path}
+              className="text-white/50 hover:text-white transition"
+            >
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          ))}
+          <li>
+            <Link href="/events/all">All Events</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
