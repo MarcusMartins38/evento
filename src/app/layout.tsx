396e519205c8133c3ab3f4c60e339115bfe07b24
unknown <marcusmartins38@gmail.com>
2024-07-27 00:40:26 -0300

    style: home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import Container from "@/components/container";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-950 text-white`}>
-        <Header />
-        {children}
-        <Footer />
+      <body className={`${inter.className} bg-gray-950 text-white h-[100vh]`}>
+        <Container>
+          <Header />
+          {children}
+          <Footer />
+        </Container>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,28 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col items-center px-3 pt-36">
       <h1>Find events around you</h1>
-      <p>Browse more then 10,000 events around you</p>
 
-      <form>
-        <input placeholder="Search events in any city..." spellCheck={false} />
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
+        Browse more than{" "}
+        <span className="font-bold text-[#a4f839] italic underline">
+          10,000 events
+        </span>{" "}
+        around you
+      </p>
+
+      <form className="w-full sm:w-[580px]">
+        <input
+          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10"
+          placeholder="Search events in any city..."
+          spellCheck={false}
+        />
       </form>
 
-      <section>
+      <section className="mt-4 flex gap-x-4 text-sm text-white/50">
         <p>Popular:</p>
-        <div>
+        <div className="space-x-2 font-semibold">
           <Link href="/events/austin">Austin</Link>
           <Link href="/events/seattle">Seattle</Link>
         </div>
