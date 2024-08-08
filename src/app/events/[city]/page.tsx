db2823ea7fd04c3cd4be6e52b72a7c5dc1648ae4
Marcus Martins <marcusmartins38@gmail.com>
2024-08-07 21:30:48 -0400

    refactor: use suspense to fetch

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,7 +1,7 @@
 import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
-import { EventoEvent } from "@/lib/types";
-import React from "react";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 type EventsPageProps = {
   params: {
@@ -11,10 +11,6 @@ type EventsPageProps = {
 
 export default async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
-  const events: EventoEvent[] = await response.json();
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -24,7 +20,9 @@ export default async function EventsPage({ params }: EventsPageProps) {
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
